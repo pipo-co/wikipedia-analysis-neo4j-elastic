17aa8a042e23ace65f3ab83623a7af01e607c9d1
igvazquez <igvazquez@itba.edu.ar>
2021-07-05 23:58:27 -0300

    Added connection with elasticsearch

--- main.py
+++ main.py
@@ -40,6 +40,8 @@ async def setup(request: Request, elastic_ip: str = Form(...), elastic_port: int
     neo_parameters["ip"] = neo_ip
     neo_parameters["port"] = neo_port
     
+    es = Elasticsearch(HOST=elastic_parameters["ip"],PORT=elastic_parameters["port"])
+
     return RedirectResponse('/import-parameters', status_code=status.HTTP_302_FOUND)
 
 @app.get("/import-parameters")
@@ -62,3 +64,20 @@ async def import_parameters(center_param: str = Form(...), lang_param: str = For
             "elastic": elastic_parameters["ip"]+':'+str(elastic_parameters["port"]),
             "neo4j": neo_parameters["ip"]+':'+str(neo_parameters["port"]),
     }
+
+@app.get("/test-elastic-post")
+def elastic_test_post(request: Request):
+    document = {
+        "description": "this is a test",
+        "timestamp": datetime.now()
+    }
+    index = "testing"
+    doc_id = 1
+    es.index(index=index, doc_type="test", id=doc_id, body=document)
+    return document
+
+@app.get("/test-elastic-get")
+def elastic_test_get(request: Request):
+    result = es.get(index="testing", doc_type="test", id=1)
+    retrieved_document = result['_source']
+    return "Retrieved document: {Id: " + result['_id'] + ", Description: " + retrieved_document['description'] + ", Timestamp: " +retrieved_document['timestamp'] + "}"
