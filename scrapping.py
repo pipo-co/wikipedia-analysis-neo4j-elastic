6a3208968097210e8d297dd1027bd35a5cb01498
tobias <tbrandy@itba.edu,ar>
2021-07-10 00:54:50 -0300

    Wikimedia api

--- scrapping.py
+++ scrapping.py
@@ -6,14 +6,14 @@ from collections import deque
 from typing import Optional, Dict, Deque, List
 
 from elasticsearch import Elasticsearch
-from mediawiki import MediaWiki, MediaWikiPage
+from mediawiki import MediaWiki, MediaWikiPage, PageError, DisambiguationError
 
 from models import ImportArticleNode
 from neo4j_repo import Neo4jRepository
 
 
 def main():
-    import_wiki("Python (programming language)", 2, 5)
+    import_wiki("Python (programming language)", 2)
 
     # def import_wiki2(center_title: str, radius: int, first_n_links: int, lang: Optional[str] = None, categories: Optional[List[str]] = None) -> None:
     #     center_page: WikipediaPage = wikipedia.page(center_title, auto_suggest=False)
@@ -24,10 +24,8 @@ def main():
     #         print(link, total)
     #         total += 1
 
-def import_wiki(center_title: str, radius: int, first_n_links: int, lang: Optional[str] = None, categories: Optional[List[str]] = None) -> None:
-    # if lang:
-    #     wikipedia.set_lang(lang)
-    wikipedia = MediaWiki()
+def import_wiki(center_title: str, radius: int, lang: str = 'en', categories: Optional[List[str]] = None) -> None:
+    wikipedia = MediaWiki(lang=lang, user_agent='neo_elastic_scraper; tbrandy@itba.edu.ar')
 
     # es = Elasticsearch(HOST=elastic_parameters["ip"], PORT=elastic_parameters["port"])
     neo = Neo4jRepository('localhost', '7687', "neo4j", "tobias")
@@ -77,26 +75,43 @@ def import_wiki(center_title: str, radius: int, first_n_links: int, lang: Option
 
                 for future in concurrent.futures.as_completed(futures):
                     # Se termino de completar el request -> Intentamos crear el nodo
-                    page: MediaWikiPage = future.result()
+                    try:
+                        page: MediaWikiPage = future.result()
+                    except (PageError, DisambiguationError) as e:
+                        print(f'Couldn\'t find link {e.title} - Ignoring page from now on')
+                        title_dist_dict[e.title] = -1
+                        continue
+
+                    pageid: int = int(page.pageid)
+
+                    if page.title in title_dist_dict:
+                        # No deberia haberse ejecutado el futuro ???
+                        continue
+
+                    if pageid == 0:
+                        print('wat')
 
                     # Para crearlo se debe cumplir que:
                     # 1. current_dist < radius porque sino estariamos creando un nodo en radio + 1
                     # 2. De haberse especificado categorias, alguna de ellas debe estar en page
+                    # 3. Id 0 es invalido
                     if (
                         current_dist < radius and
-                        (categories is None or any(cat in page.categories for cat in categories))
+                        (categories is None or any(cat in page.categories for cat in categories)) and
+                        pageid != 0
                     ):
                         # Creo nodo y relacion en neo
-                        if not neo.create_and_link_article(current_node.id, int(page.pageid), page.title, page.categories, neo_session):
+                        if not neo.create_and_link_article(current_node.id, pageid, page.title, page.categories, neo_session):
                             raise Exception('Un nodo que pense que tenia que crear, ya esta creado')
 
                         # Pongo el nuevo nodo en las estructuras
                         title_dist_dict[page.title] = current_dist + 1
-                        node_q.append(ImportArticleNode(int(page.pageid), page.title, page.links))
+                        node_q.append(ImportArticleNode(pageid, page.title, page.links))
 
                     # Si no cumple, lo agrego con dist -1 para no volver a calcularlo
                     else:
                         title_dist_dict[link] = -1
+                        continue
 
                     resolved_count += 1
                     print(f'({current_node.title})---({page.title}). Total: {resolved_count}')
